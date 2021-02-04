4bc3b2ab11060e61892e3fdd6c4fb72565462fd0
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-04 12:38:47 +0100

    Modifyng the fetchUser statement. Looking for unique ids using lodash and learning how the chain() function makes it work in a more clear way

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    /* 
+    _.chain(getState(posts)
+        .map('userId)
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+    */
 };
 
 export const fetchPosts = () => async dispatch => {
